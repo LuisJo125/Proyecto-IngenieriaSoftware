832559f2af2416cf0fbc51a0325648b2bd0054d5
Luis José Villalobos Camareno <39417849+LuisJo125@users.noreply.github.com>
2024-05-02 14:46:16 -0600

    Creacion de proyecto
    Luis José Villalobos

---
+++ SIERRHH/SIERRHH/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SIERRHH.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
