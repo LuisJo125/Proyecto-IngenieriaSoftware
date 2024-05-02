e599ecd023d152caec96ec32e1f0262b946a5a25
Luis José Villalobos Camareno <39417849+LuisJo125@users.noreply.github.com>
2024-05-02 15:13:15 -0600

    ss
    ss

--- SIERRHH/SIERRHH/obj/Debug/net8.0/SIERRHH.AssemblyInfo.cs
+++ SIERRHH/SIERRHH/obj/Debug/net8.0/SIERRHH.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SIERRHH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc6450ede4d20404a32b260a20ecb766d3ee5256")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4a02a7a97d7612e857f36a385c8b0bf560546fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SIERRHH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SIERRHH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
