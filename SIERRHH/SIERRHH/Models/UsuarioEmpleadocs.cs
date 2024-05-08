ae6ce05af98d5c42b49dfef76a4712b80a416dcb
Luis José Villalobos Camareno <39417849+LuisJo125@users.noreply.github.com>
2024-05-07 20:38:05 -0600

    ss
    ss

---
+++ SIERRHH/SIERRHH/Models/UsuarioEmpleadocs.cs
@@ -0,0 +1,14 @@
+﻿namespace SIERRHH.Models
+{
+    public class UsuarioEmpleadocs
+    {
+        public int IdEmpleado { get; set; }
+        public string Correo { get; set; }
+
+        public string Password { get; set; }
+
+        public string Rol { get; set; }
+
+        public string Estado { get; set; }
+    }
+}
--- SIERRHH/SIERRHH/obj/Debug/net8.0/SIERRHH.AssemblyInfo.cs
+++ SIERRHH/SIERRHH/obj/Debug/net8.0/SIERRHH.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SIERRHH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4a02a7a97d7612e857f36a385c8b0bf560546fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ce39250d9a0e484345b5c97e3de5d833e7b2961")]
 [assembly: System.Reflection.AssemblyProductAttribute("SIERRHH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SIERRHH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
